c61c9bc85dc812ad777bda0de121701e35f5d87a
harrisonzhu508 <harrisonzhu5080@gmail.com>
2019-10-17 14:53:51 +0100

    add in jupyter int and distributions module

---
+++ src/sampler.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.random import normal
+
+class FiniteDirichlet:
+    """FiniteDirichlet class
+
+    Args:
+        
+        - parameters: dictionary containing alpha, M
+        - prior:
+        - likelihood:
+    """
+    def __str__(self):
+        print_str = "Finite Dirichlet Mixture model with self.parameters:"
+        return print_str
+
+    def __init__(self, parameters, prior, likelihood):
+
+        self.parameters = parameters
+        self.prior = prior
+        self.likelihood = likelihood
+
+    def run_chain(self):
+        pass
+
+
+
+
+
+
