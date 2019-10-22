725ddfdf77ee704e9e7f78f4a25c8ba4b4238ac6
harrisonzhu508 <harrisonzhu5080@gmail.com>
2019-10-22 20:52:52 +0100

    change args

--- src/plot.py
+++ src/plot.py
@@ -5,46 +5,84 @@ import matplotlib.pyplot as plt
 # J - num samples
 # SS - subsample iterations
 
-def plot_co_occurance_matirx(assignment_matrix):
-    Nsamp = assignment_matrix.shape[0] # Number of samples taken
-    nd = assignment_matrix.shape[1] # Number of datapoints
-    com = np.zeros([nd, nd]) # empty co-occurance matrix
+
+def plot_co_occurrence_matrix(
+    assignment_matrix, file_dir="../results", filename="co_occurrence_matrix"
+):
+    """plot the co-occurrence matrix i.e. p(i,j) = prob i,j in same cluster 
+    i, j being the ith and jth datapoints in the dataset
+
+    Very slow! 
+    T(n) = T(Nsamp*n) + O(1) complexity
+
+    """
+    Nsamp = assignment_matrix.shape[0]  # Number of samples taken
+    nd = assignment_matrix.shape[1]  # Number of datapoints
+    com = np.zeros([nd, nd])  # empty co-occurrence matrix
 
     for i in range(nd):
-        for j in range(i+1):
+        for j in range(i + 1):
             for m in range(Nsamp):
-                com[i,j] = com[i,j] + (assignment_matrix[m, i] == assignment_matrix[m, j])
+                com[i, j] = com[i, j] + (
+                    assignment_matrix[m, i] == assignment_matrix[m, j]
+                )
 
     for i in range(nd):
-        for j in range(i+1):
-            com[j,i] = com[i,j]
+        for j in range(i + 1):
+            com[j, i] = com[i, j]
 
     com = com / Nsamp
-    
-    plt.figure()
-    ax = sns.heatmap(com, square=True)
-    ax.invert_yaxis()
 
-def plot_cluster_size_hist(assignments):
+    plt.figure()
+    ax = sns.heatmap(com, square=False)
+    ax.invert_yaxis()
+    plt.title("Co-occurrence matrix i.e. p(i,j) = prob i,j in same cluster")
+    plt.ylabel("Datapoints")
+    plt.xlabel("Datapoints")
+    plt.savefig("{}/{}.png".format(file_dir, filename))
+
+
+def plot_cluster_size_hist(
+    assignments, file_dir="../results", filename="cluster_size_hist"
+):
+    """
+
+    """
     Nsamp = assignments.shape[0]
     nd = assignments.shape[1]
 
     num_clusters = np.array(list(map(lambda x: len(set(x)), assignments)))
 
     plt.figure()
-    sns.distplot(num_clusters, bins = np.array(list(range(np.max(num_clusters)+2))) - 0.5, kde=False, hist_kws={'ec': 'k'})
+    sns.distplot(
+        num_clusters,
+        bins=np.array(list(range(np.max(num_clusters) + 2))) - 0.5,
+        kde=False,
+        hist_kws={"ec": "k"},
+    )
+    plt.title("Histogram of the cluster occurrence")
+    plt.xlabel("Cluster")
+    plt.ylabel("Frequency")
+    plt.savefig("{}/{}.png".format(file_dir, filename))
 
-def plot_cluster_params(mus, weights):
-    print(mus)
 
-    vec_mus = np.concatenate(mus)
-    vec_weights = np.concatenate(weights)
+def plot_cluster_params(mu_chain, weights_chain, file_dir="../results", filename="cluster_para"):
+    """Plots x-y graph with x being the parameters and y being the weights
+    we plot it with the colour representing the number of clusters
+    
+    """
+
+    mu_flat = np.concatenate(mu_chain).ravel()
+    weights_flat = np.concatenate(weights_chain)
 
     num_clusters = [len(mu) for mu in mus]
     num_clusters = [np.ones(n) * n for n in num_clusters]
-    vec_num_clusters = np.concatenate(num_clusters)
 
     sns.scatterplot(vec_mus, vec_weights, hue=num_clusters)
+    plt.savefig("{}/{}.png".format(file_dir, filename))
 
-def plot_posterior_predictive(mus, sigmas, weights):
+
+def plot_posterior_predictive(
+    mus, sigmas, weights, file_dir="../results", filename="cluster_size_hist"
+):
     pass
