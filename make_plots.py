f050fb217662dceaeb9228f7d406f2ed2fa2348b
harrisonzhu508 <harrisonzhu5080@gmail.com>
2019-10-23 17:38:48 +0100

    fix plots script

--- make_plots.py
+++ make_plots.py
@@ -12,11 +12,11 @@ parser.add_argument('-r', '--results_dir', required=True)
 parser.add_argument('-b', '--burn_in', default=100, type=int)
 parser.add_argument('-t', '--thinning', default=10, type=int)
 
-args = parser.parse_args()
-results_dir = args.results_dir 
+# args = parser.parse_args()
+# results_dir = args.results_dir 
+results_dir = "/Users/harrisonzhu/Documents/work/code/dirichlet-bayes/results/finite/galaxy_N_10000_M_3_alpha_1.000_m0_20.000_s0_10.000_a0_2.000_b0_0.111"
 # Load data from file
 data = np.loadtxt("data/galaxy.txt")
-results_dir = args.results_dir
 assignments = np.load(os.path.join(results_dir, 'assignments.npy'))
 chain = pickle.load(open(os.path.join(results_dir, 'chain_iter.pkl'), 'rb'))
 mus = chain["mu"]
@@ -24,8 +24,10 @@ sigmas =  chain["sigma"]
 weights = chain["weights"]
 
 # Thin the samples to reduce correlation
-burn_in = args.burn_in
-thin_factor = args.thinning
+# burn_in = args.burn_in
+# thin_factor = args.thinning
+burn_in = 1000
+thin_factor = 100
 
 assignments = assignments[burn_in::thin_factor]
 mus = mus[burn_in::thin_factor]
